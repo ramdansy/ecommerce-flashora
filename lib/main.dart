12f4f7f7b3047270644fef62f2cc19eeead52a73
Ramdan Syaripudin <macbook@Macbook.local>
2024-08-20 22:45:45 +0700

    add env

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'core/common/utils/fcm_helper.dart';
 import 'core/common/utils/notification_helper.dart';
@@ -20,9 +21,11 @@ import 'presentation/routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await NotificationHelper().initLocalNotifications();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FcmHelper().init();
+
   setupLocator();
 
   runApp(const MyApp());
